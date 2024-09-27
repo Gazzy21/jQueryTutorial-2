f71f65b9768a1a8c828d1fbe229d00b9f28c7ff6
Gazzy21 <nicholas221@icloud.com>
2024-09-27 14:36:51 -0400

    finished tutorial

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.swatch').on('click', function() {
+        $('#product-photo').attr('src', `img/chair-${this.id}@2x.jpg`);
+        $('.swatch').removeClass
+    });
+});
